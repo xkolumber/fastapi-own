2c91c37caba59b4accc257c8d0472c0de7ddaeed
xkolumber <lubosk158@gmail.com>
2025-10-04 23:41:47 +0200

    deleted phone number

--- app/models.py
+++ app/models.py
@@ -22,7 +22,6 @@ class User(Base):
     email = Column(String, nullable=False, unique=True )
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default= text('now()'))
-    phone_number = Column(String)
 
 class Vote(Base):
     __tablename__ = "votes"
